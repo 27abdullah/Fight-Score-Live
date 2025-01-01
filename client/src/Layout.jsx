e908395cedcafe6897c361b829d8dd2c1d26e051
27abdullah <abdullahbadat27@gmail.com>
2025-01-02 00:03:28 +1000

    Positioned navbar at top of screen

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -3,11 +3,11 @@ import { Navbar } from "./components/navbar";
 
 export function Layout() {
     return (
-        <>
+        <div className="flex flex-col h-screen">
             <Navbar />
-            <main>
+            <main className="flex-grow flex items-center justify-center">
                 <Outlet />
             </main>
-        </>
+        </div>
     );
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
     return (
-        <>
-            <Link to="/">
-                <button>Home</button>
-            </Link>
-            <Link to="/score-page">
-                <button>ScorePage</button>
-            </Link>
-        </>
+        <nav className="flex items-center justify-center px-4 pt-4">
+            <div className="flex space-x-4">
+                <Link to="/">
+                    <button className="px-4 pt-2 ">Home</button>
+                </Link>
+                <Link to="/score-page">
+                    <button className="px-4 pt-2 ">ScorePage</button>
+                </Link>
+            </div>
+        </nav>
     );
 }
