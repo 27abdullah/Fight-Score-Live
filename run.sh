bced9179567fa385832c559ca32bedbe0f1f9173
27abdullah <abdullahbadat27@gmail.com>
2025-01-12 18:48:36 +1000

    Added run script

---
+++ run.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+function start() {
+    
+    cd server
+    docker-compose up -d
+    npm run dev &
+    SERVER_PID=$!
+
+    cd ../client
+    npm run dev &
+    CLIENT_PID=$!
+
+    cd ../
+    trap stop INT
+    
+    wait $SERVER_PID
+    wait $CLIENT_PID
+}
+
+function stop() {
+    if [ -n "$SERVER_PID" ]; then
+        kill $SERVER_PID
+        wait $SERVER_PID 2>/dev/null
+    fi
+    
+    if [ -n "$CLIENT_PID" ]; then
+        kill $CLIENT_PID
+        wait $CLIENT_PID 2>/dev/null
+    fi
+    
+    cd server
+    docker-compose down
+    
+    echo "Stopped Fight-Score-Live"
+    exit 0
+}
+
+case $1 in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
