7999c21c2674399699d48615985bb52cf79a8117
27abdullah <abdullahbadat27@gmail.com>
2025-06-12 22:03:14 +1000

    Aesthetics

--- client/src/components/ScorePage/NameTag.jsx
+++ client/src/components/ScorePage/NameTag.jsx
@@ -2,8 +2,8 @@ export function NameTag({ name, id, isWinner }) {
     const winnerClass = isWinner === id ? "text-green-500" : "";
     const shadow =
         id === "A"
-            ? `drop-shadow-[0_0_10px_rgba(255,0,0,0.3)]`
-            : `drop-shadow-[0_0_10px_rgba(0,0,255,0.3)]`;
+            ? `drop-shadow-[0_0_10px_rgba(255,0,0,0.5)]`
+            : `drop-shadow-[0_0_10px_rgba(0,0,255,0.5)]`;
     return (
         <div className="flex items-center justify-center w-full h-full">
             <h1
