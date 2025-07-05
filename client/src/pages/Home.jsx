ab4d1d30a2f189ef6beb5b8653cdee4236761841
27abdullah <abdullahbadat27@gmail.com>
2025-07-06 09:52:36 +1000

    animate bug fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ export function Home() {
     return (
         <>
             <div className="text-center animate-fade-in">
-                <h1 className="font-industry_demi text-5xl sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r animate-scale-in from-white via-rich_carmine to-red-700 bg-clip-text text-transparent mb-4 animate-title-pulse">
+                <h1 className="font-industry_demi text-5xl sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r from-white via-rich_carmine to-red-700 bg-clip-text text-transparent mb-4 animate-title-pulse">
                     Fight Score Live
                 </h1>
                 <p className="text-lg mt-2 font-industry_demi sm:text-xl md:text-2xl lg:text-3xl text-white animate-scale-in">
