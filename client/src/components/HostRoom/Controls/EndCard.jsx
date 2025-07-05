42a69f4f3c121a3c32a78219f3a2f7ee63f3f956
27abdullah <abdullahbadat27@gmail.com>
2025-07-06 09:51:58 +1000

    Landing page update

--- client/src/components/HostRoom/Controls/EndCard.jsx
+++ client/src/components/HostRoom/Controls/EndCard.jsx
@@ -1,7 +1,7 @@
 export default function EndCard({ handleRequest }) {
     return (
         <button
-            className="px-4 py-2 bg-blue-600 hover:bg-blue-700 rounded"
+            className="px-4 py-2 bg-rich_carmine hover:bg-red-900 rounded"
             onClick={() => handleRequest("end-card", "POST", {})}
         >
             End Card
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,14 @@
 export function Home() {
     return (
         <>
-            <h1 className="font-inter_regular">Fight Score Live</h1>
+            <div className="text-center animate-fade-in">
+                <h1 className="font-industry_demi text-5xl sm:text-5xl md:text-6xl lg:text-7xl bg-gradient-to-r animate-scale-in from-white via-rich_carmine to-red-700 bg-clip-text text-transparent mb-4 animate-title-pulse">
+                    Fight Score Live
+                </h1>
+                <p className="text-lg mt-2 font-industry_demi sm:text-xl md:text-2xl lg:text-3xl text-white animate-scale-in">
+                    Your Voice. Your Winner.
+                </p>
+            </div>
         </>
     );
 }
